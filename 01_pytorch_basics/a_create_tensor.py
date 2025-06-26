22d6b7e2d459c58407e1f5b3a31f7a54a8315cee
zwphhxx <zwphhxx@163.com>
2025-06-26 13:59:10 +0800

    2-commit-code

--- 01_pytorch_basics/a_create_tensor.py
+++ 01_pytorch_basics/a_create_tensor.py
@@ -33,4 +33,36 @@ print(f"int_tensor->{int_tensor}") # DeprecationWarning: an integer is required 
 print(f"torch.ShortTensor()->{torch.ShortTensor([3,2])}") # tensor([], dtype=torch.int16)
 print(f"torch.LongTensor()->{torch.LongTensor([3,2])}") # tensor([], dtype=torch.int64)
 print(f"torch.FloatTensor()->{torch.FloatTensor([3,2])}") # tensor([], dtype=torch.float32)
-print(f"torch.DoubleTensor()->{torch.DoubleTensor([3,2])}") # tensor([], dtype=torch.float64)
+print(f"torch.DoubleTensor()->{torch.DoubleTensor([3,2])}\n") # tensor([], dtype=torch.float64)
+
+# Create a linear tensor.
+linear_tensor_arange = torch.arange(1,15,3)
+print(f"linear_tensor_arange->\n{linear_tensor_arange}")
+linear_tensor_linspace = torch.linspace(13,22,3) # Evenly split
+print(f"linear_tensor_linspace->\n{linear_tensor_linspace}\n")
+
+# Create a random tensor.
+randn_tensor = torch.randn(2,4)
+print(f"randn_tensor->\n{randn_tensor}")
+seed = torch.random.initial_seed()
+print(f"torch random seed->\n{seed}") # 15646168311400
+
+torch.manual_seed(seed)
+specified_randn_tensor = torch.randn(2,4)
+print(f"specified_randn_tensor->\n{specified_randn_tensor}\n")
+
+# Create an zeros / ones / full tensor.
+ones_tensor = torch.ones(2,4)
+print(f"ones_tensor->\n{ones_tensor}")
+ones_like_torch = torch.ones_like(ones_tensor)
+print(f"ones_like_torch->\n{ones_like_torch}")
+
+zeros_tensor = torch.zeros(2,4)
+print(f"zeros_tensor->\n{zeros_tensor}")
+zeros_like_torch = torch.zeros_like(int_matrix_tensor)
+print(f"zeros_like_torch->\n{zeros_like_torch}")
+
+full_tensor = torch.full((4,5),30)
+print(f"full_tensor->\n{full_tensor}")
+full_like_tensor = torch.full_like(zeros_tensor,50)
+print(f"full_like_tensor->\n{full_like_tensor}")
